bdd02dd10cba132b177559b3c1f63888fd9d4bee
Josef Vybíral <josef.vybiral@dobra-rada.cz>
2013-08-28 12:42:58 +0200

    Skript na náhledy je teď kešuje (z minigal 0.3.7). + Malá úprava textu po přihlášení.

--- includes/_loginsuccess.inc.php
+++ includes/_loginsuccess.inc.php
@@ -1,7 +1,7 @@
 <div class="loginform">
     <div class="row">
         <div class="large-12 columns">
-            <p>Prihlásenie bolo úspešné, teraz máte prístup do <a href="/forum">fóra</a> a sekcie <a href="/na-stiahnutie">na stiahnutie</a> :-)</p>
+            <p>Od teraz máte prístup do <a href="/forum">fóra</a> a sekcie <a href="/na-stiahnutie">na stiahnutie</a> alebo pre <a href="/galeria?dir=Vaše%20fotky">nahrávanie fotiek</a> do galérie Vaše fotky :-)</p>
         </div>
     </div>
 </div>
