7faff673acb721f055978f7a9ec9cee7b6801b24
Josef Vybíral <josef.vybiral@dobra-rada.cz>
2013-10-09 09:53:54 +0200

    lint javascriptu

--- assets/js/jquery.equalHeights.js
+++ assets/js/jquery.equalHeights.js
@@ -1,17 +1,22 @@
-$.fn.equalHeights = function(px) {
-    var grid = 16;
-    var heights = new Array ();
+(function ($) {
 
-    Array.max = function ( array ){
-        return Math.max.apply( Math, array );
+    "use strict";
+
+    $.fn.equalHeights = function (px) {
+        var grid = 16,
+            heights = [];
+
+        Array.max = function (array) {
+            return Math.max.apply(Math, array);
+        };
+
+        $(this).each(function (index) {
+            heights.push($(this).height());
+        });
+
+        var height = Array.max(heights),
+            diff = height % grid;
+
+        $(this).height(height + diff);
     };
-
-    $(this).each(function(index){
-        heights.push($(this).height());
-    });
-
-    var height = Array.max(heights),
-        diff = height % grid;
-
-    $(this).height(height + diff);
-}
+})(jQuery);
