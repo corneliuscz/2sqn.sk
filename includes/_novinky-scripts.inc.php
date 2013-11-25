b9ebf6561f178519f42d1058bdd399c17369697c
Josef Vybíral <josef.vybiral@dobra-rada.cz>
2013-11-25 16:43:13 +0100

    Oprava šířky novinky pokud je první přes 2 sloupce.

--- includes/_novinky-scripts.inc.php
+++ includes/_novinky-scripts.inc.php
@@ -5,6 +5,7 @@ $(function(){
     var $container = $('#masonry .row');
 
     $container.masonry({
+        columnWidth: '.large-6',
         itemSelector: '.novinka-text',
         isAnimated: true
     });
